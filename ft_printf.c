b136a22eb129e059d83b6fe900b37b6028825354
Gabriel <grspes_05@hotmail.com>
2025-08-06 17:26:40 -0300

    att

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,26 @@
 /*   By: grodrig2 <grodrig2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:49:51 by grodrig2          #+#    #+#             */
-/*   Updated: 2025/08/06 14:51:22 by grodrig2         ###   ########.fr       */
+/*   Updated: 2025/08/06 17:10:58 by grodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *format, ...)
 {
-	int i;
+	int		total;
+	va_list arg;
+
+	va_start(arg, format);
+	total = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			print_format(*(++format), arg);
+		}
+	}
+
+	va_end (arg);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,16 +6,18 @@
 /*   By: grodrig2 <grodrig2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 18:16:33 by grodrig2          #+#    #+#             */
-/*   Updated: 2025/08/06 14:52:51 by grodrig2         ###   ########.fr       */
+/*   Updated: 2025/08/06 16:10:48 by grodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
+#include<unistd.h>
+#include<stdlib.h>
 #include<stdarg.h>
 
-int	ft_printf(const char *, ...);
+int		ft_printf(const char *, ...);
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 void	ft_putnbr_base(int n, char c);
